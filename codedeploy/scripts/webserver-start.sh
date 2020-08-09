7bab6b595fc89655da6637b225a89694deff44a6
Ryan Warrener <Ryan.Warrener@intrepidtravel.com>
2020-08-09 18:26:06 +1000

    Using appspec file to copy .service file into systemd dir

--- codedeploy/scripts/webserver-start.sh
+++ codedeploy/scripts/webserver-start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-pwd
-cp podcast-land.service /etc/systemd/system/podcast-land.service
-chmod 644 /etc/systemd/system/podcast-land.service
 systemctl enable podcast-land
 systemctl start podcast-land
 service nginx start
