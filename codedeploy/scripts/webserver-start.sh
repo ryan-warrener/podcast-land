bba1babb8437cfd00f89cc6f14777dd5d52e89d1
Ryan Warrener <Ryan.Warrener@intrepidtravel.com>
2020-08-09 18:05:39 +1000

    copied service file into systemd folder so that service can be enabled and started

--- codedeploy/scripts/webserver-start.sh
+++ codedeploy/scripts/webserver-start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-systemctl enable podcast-land.service
+cp podcast-land.service /etc/systemd/system/podcast-land.service
+chmod 644 /etc/systemd/system/podcast-land.service
+systemctl enable podcast-land
+systemctl start podcast-land
 service nginx start
